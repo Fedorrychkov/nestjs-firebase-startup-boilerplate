ba9bf254910bd62c3b8e73c795d1830677e4971b
Fedor Rychkov <fedorrychkov@yandex.ru>
2024-07-27 22:00:24 +0300

    chore: add admin package initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { getEnvFile } from './env'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: getEnvFile(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import * as admin from 'firebase-admin'
+import * as fs from 'fs'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const configService: ConfigService = app.get(ConfigService)
+  const accountPath = configService.get<string>('SA_KEY')
+  const serviceAccount: any = JSON.parse(fs.readFileSync(accountPath, 'utf8'))
+
+  const adminConfig: admin.ServiceAccount = {
+    projectId: serviceAccount.project_id,
+    privateKey: serviceAccount.private_key,
+    clientEmail: serviceAccount.client_email,
+  }
+
+  admin.initializeApp({
+    credential: admin.credential.cert(adminConfig),
+    databaseURL: `https://${adminConfig.projectId}.firebaseio.com`,
+  })
+
+  await app.listen(8080)
 }
+
 bootstrap()
